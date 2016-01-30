d70a91ea484fcd157ba06268b380ee73091ac71d
Daniel Boisselle <dboisselleucf@gmail.com>
2016-01-29 21:26:07 -0500

    ScoreManager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	private int score = 0;
+	private int health = 0;
+	private int noteStreak = 0;
+
+//	string PERFECT = "perfect";
+//	string OK = "ok";
+//	string BAD = "bad";
+//	string MISSED = "missed";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getScore(){
+
+		return score;
+	}
+
+	public int getHealth(){
+
+		return health;
+
+	}
+
+	public int getNoteStreak (){
+		return noteStreak;
+
+	}
+		
+	public void hitProgress (string hitType){
+
+		switch(hitType){
+
+		case "perfect":
+		case "ok":
+			
+			goodHit(hitType);
+			Debug.Log(" goodHit method :::: \n" + score + "\n" + health + "\n" + noteStreak + "\n");
+
+			break;
+
+		case "bad": 
+		case "missed":
+			
+			badHit(hitType);
+			Debug.Log(" badHit method :::: \n" + score + "\n" + health + "\n" + noteStreak + "\n");
+
+			break;
+
+		default:
+			
+			Debug.Log ("Problems passing the hitType");
+
+			break;
+
+		}
+
+		Debug.Log("\n" + score + "\n" + health + "\n" + noteStreak + "\n");
+	
+	}
+
+	public void goodHit(string hitType){
+
+		if (hitType.Equals ("perfect")) {
+
+			score += 10;
+			health += 2;
+
+		} else {
+			
+			score += 5;
+			health++;
+
+		}
+
+		noteStreak++;
+
+	}
+
+	public void badHit(string hitType ) {
+
+		if (hitType.Equals("bad")) {
+
+			score++;
+
+		} else {
+
+
+			if (score != 0 || score >= 5 ) score -= 5;
+
+			if (health != 0 || health >= 2 ) health -= 2;
+
+		}
+
+		noteStreak = 0;
+	
+	}
+
+}
---
+++ Assets/Scripts/tester.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tester : MonoBehaviour {
+
+	ScoreManager scoreman;
+
+	// Use this for initialization
+	void Start () {
+	
+		scoreman = GameObject.FindGameObjectWithTag ("test").GetComponent<ScoreManager>();
+
+		scoreman.hitProgress ("bad");
+		scoreman.hitProgress ("perfect");
+		scoreman.hitProgress ("ok");
+		scoreman.hitProgress ("missed");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	//  B   p    o   m
+//	s = 1 + 10 + 5 + -5 === 11
+//	h = 0 + 2  + 1 + -2 === 1
+//	n = 0 + 1  + 1 = 0  === 0
+
+
+}
